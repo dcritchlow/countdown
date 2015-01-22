aea6f5882cf5e0ff8d2884a8c81e1f38cb710c01
Darin Critchlow <darincritchlow@gmail.com>
2015-01-22 12:36:55 -0700

    Assignment Completed

--- js/countdown.js
+++ js/countdown.js
@@ -1,17 +1,45 @@
-// Function to complete.
 function CountDownTimer(dateTime, divId) {
+
     var end     = new Date(dateTime);
+    var test    = new Date();
     var _second = 1000;
     var _minute = _second * 60;
     var _hour   = _minute * 60;
     var _day    = _hour * 24;
     var timer;
 
-    function showRemaining() {
-       // ... 
+    function getDaysLeft(milliseconds) {
+      return Math.floor(new Date(milliseconds) / _day % 24) + ' days, ';
     }
 
-    timer = setInterval(showRemaining(), 1000);
+    function getHoursLeft(milliseconds) {
+      return Math.floor(new Date(milliseconds) / _hour % 60) + ' hrs, ';
+    }
+
+    function getMinutesLeft(milliseconds) {
+      return Math.floor(new Date(milliseconds) / _minute % 60) + ' mins, ';
+    }
+
+    function getSecondsLeft(milliseconds) {
+      return Math.floor(new Date(milliseconds) / _second % 60) + ' secs ';
+    }
+
+    function showRemaining() {
+
+       if ( Date.now() < end.getTime() ) {
+         document.getElementsByName('countDown')[0].value =
+          getDaysLeft(end.getTime() - Date.now()) +
+          getHoursLeft(end.getTime() - Date.now()) +
+          getMinutesLeft(end.getTime() - Date.now()) +
+          getSecondsLeft(end.getTime() - Date.now());
+       }
+
+       else {
+         document.getElementsByName('countDown')[0].value = "Can't countdown from something that I've passed..."
+       }
+    }
+
+    timer = setInterval(showRemaining, 1000);
 }
 
 // Enter the date and time as a string in the following format.  Second parameter is the HTML id of element where you want
